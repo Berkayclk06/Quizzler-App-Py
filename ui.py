21471b52c9fe2b9c1b9296148ca34557c3d6e7e9
Berkay Colak <berkaycolak42@gmail.com>
2022-09-14 03:53:52 -0400

    final ui commit

--- ui.py
+++ ui.py
@@ -1,26 +1,36 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
-        self.score = Label(text="Score: ", bg=THEME_COLOR, fg="white")
+
+        self.score = Label(text="Score: 0", bg=THEME_COLOR, fg="white")
         self.score.grid(row=0, column=1)
-        self.score.config(padx=20, pady=20)
-        self.canvas = Canvas(width=300, height=250)
-        self.question_text = self.canvas.create_text(150, 125, text=f"", width=250,
-                                                     font=("Arial", 20, "italic"), fill="white")
-        self.canvas.grid(row=1, column=0, columnspan=2)
-        self.canvas.config(padx=20, pady=20)
-        self.true_img = PhotoImage(file="images/true.png")
-        self.false_img = PhotoImage(file="images/false.png")
-        self.true_btn = Button(image=self.true_img, highlightthickness=0)
-        self.false_btn = Button(image=self.false_img, highlightthickness=0)
+
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(150, 125, text="Some text", width=280,
+                                                     font=("Arial", 20, "italic"), fill=THEME_COLOR)
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+        true_img = PhotoImage(file="images/true.png")
+        false_img = PhotoImage(file="images/false.png")
+        self.true_btn = Button(image=true_img, highlightthickness=0)
         self.true_btn.grid(row=2, column=0)
+
+        self.false_btn = Button(image=false_img, highlightthickness=0)
         self.false_btn.grid(row=2, column=1)
+
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
